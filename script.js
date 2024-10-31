f476b94ee336bc9399b5a7ce40a42f1f0cbb2382
Edward Omopariola <femiedjr@gmail.com>
2024-10-31 15:17:39 -0400

    added some comment to my js file

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-let inputBox = document.getElementById("input-box");
-let listContainer = document.getElementById("list-container");
+let inputBox = document.getElementById("input-box"); // Declaring a varible of "input-box"
+let listContainer = document.getElementById("list-container"); // Declaring a varible of "list-container"
 
 function addTask() {
-    if(inputBox.value === ''){
-        alert("write something");
+    if(inputBox.value === ''){ //Creeating a inputbox with an empty string for user to write their todo list
+        alert("make a list");
     }
     else{
         let li = document.createElement("li");
         li.innerHTML = inputBox.value
         listContainer.appendChild(li);
-        let span = document.createElement("span")
-        span.innerHTML = "\u00d7"
+        let span = document.createElement("span") //creating a span element
+        span.innerHTML = "\u00d7" //
         li.appendChild(span);
     }
     inputBox.value = "";
     saveData()
 };
 
-listContainer.addEventListener("click", function(e) {
-    if(e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
+listContainer.addEventListener("click", function(e) { 
+    if(e.target.tagName === "LI") { //when a user click on li, it will check the class list
+        e.target.classList.toggle("checked"); 
         saveData()
     }
-    else if (e.target.tagName === "SPAN") {
+    else if (e.target.tagName === "SPAN") { //when a user click on span, it will delete the parent element
         e.target.parentElement.remove()
         saveData()
     }
